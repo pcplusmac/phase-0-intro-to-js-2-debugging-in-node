5e063c7d0cc43b034c9b4f99b8c029632e5a2162
pc-sam <samurai.h@yahoo.com>
2025-08-16 14:23:02 +0800

    add new debugger

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ while (count < 5) {
     console.log(`The loop has executed ${count} times.`);
   }
   count += 1;
+  // Add the second breakpoint, so that we can check both before and after the incrementation of count variable. 
+  debugger; 
 }
